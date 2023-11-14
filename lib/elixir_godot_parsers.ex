92d42268c7a8bb83a7fdebc900ac612568c8e49e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-14 01:33:25 -0800

    Refactor Transform3D and Transform2D structs

--- lib/elixir_godot_parsers.ex
+++ lib/elixir_godot_parsers.ex
@@ -3,11 +3,22 @@ defmodule Color do
 end
 
 defmodule Transform3D do
-  defstruct cell0: 0, cell1: 0, cell2: 0
+  defstruct cell0: 0,
+            cell1: 0,
+            cell2: 0,
+            cell3: 0,
+            cell4: 0,
+            cell5: 0,
+            cell6: 0,
+            cell7: 0,
+            cell8: 0,
+            cell9: 0,
+            cell10: 0,
+            cell11: 0
 end
 
 defmodule Transform2D do
-  defstruct cell0: 0, cell1: 0
+  defstruct cell0: 0, cell1: 0, cell2: 0, cell3: 0
 end
 
 defmodule ElixirGodotParsers do
