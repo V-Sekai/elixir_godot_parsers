c63700f991e6eb9b585614dd1a94d66c60be398f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-14 01:11:34 -0800

    Add more parsers.

--- lib/elixir_godot_parsers.ex
+++ lib/elixir_godot_parsers.ex
@@ -18,7 +18,7 @@ defmodule ElixirGodotParsers do
   Parses file descriptors in Godot files.
   """
   defparsec(
-    :file_descriptor,
+    :file_descriptor_parser,
     string("[gd_scene load_steps=")
     |> repeat(utf8_string([], min: 1))
     |> optional(string(" format="))
@@ -32,31 +32,97 @@ defmodule ElixirGodotParsers do
   @doc """
   Parses external resources in Godot files.
   """
-  def external_resources_parser do
-    # Define the parser for external resources
-  end
+  defparsec(
+    :external_resources_parser,
+    string("[ext_resource path=\"")
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" type=\""))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" id="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("]")),
+    debug: false
+  )
 
   @doc """
   Parses internal resources in Godot files.
   """
-  def internal_resources_parser do
-    # Define the parser for internal resources
-  end
+  defparsec(
+    :internal_resources_parser,
+    string("[sub_resource type=\"")
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" id="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("]")),
+    debug: false
+  )
 
   @doc """
   Parses connections in Godot files.
   """
-  def connections_parser do
-    # Define the parser for connections
-  end
+  defparsec(
+    :connections_parser,
+    string("[connection signal=\"")
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" from=\""))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" to=\""))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" method=\""))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\"]")),
+    debug: false
+  )
 
-  # Define a comprehensive parser for each file format
+  @doc """
+  Parses sub_resource descriptor in Godot files.
+  """
+  defparsec(
+    :sub_resource_parser,
+    string("[sub_resource type=\"")
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("\" id="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("]")),
+    debug: false
+  )
+
+  @doc """
+  Parses color in Godot files.
+  """
+  defparsec(
+    :color_parser,
+    string("[color r=")
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string(" g="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string(" b="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string(" a="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("]")),
+    debug: false
+  )
+
+  @doc """
+  Parses transform in Godot files.
+  """
+  defparsec(
+    :transform_parser,
+    string("[transform cell0=")
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string(" cell1="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string(" cell2="))
+    |> repeat(utf8_string([], min: 1))
+    |> optional(string("]")),
+    debug: false
+  )
 
   @doc """
   Parses TSCN files.
   """
   def tscn_parser do
-    # Combine the relevant parsers for TSCN files
   end
 
   @doc """
@@ -70,6 +136,5 @@ defmodule ElixirGodotParsers do
   Parses TRES files.
   """
   def tres_parser do
-    # Define the parser for TRES files
   end
 end
--- test/elixir_godot_parsers_test.exs
+++ test/elixir_godot_parsers_test.exs
@@ -5,18 +5,35 @@ defmodule ElixirGodotParsersTest do
   alias ElixirGodotParsers, as: Parser
 
   @gd_scene "[gd_scene load_steps=4 format=3 uid=\"uid://cecaux1sm7mo0\"]"
-
   @node_descriptor "[node name=\"NodeName\" type=\"NodeType\"]"
+  @sub_resource "[sub_resource type=\"YourType\" id=\"YourID\"]"
+  @color "[color r=255 g=255 b=255 a=255]"
+  @transform "[transform cell0=1 cell1=0 cell2=0]"
 
   test "parses gd_scene descriptor correctly" do
     assert {:ok, ["[gd_scene load_steps=", "4 format=3 uid=\"uid://cecaux1sm7mo0\"]"], "", %{},
             {1, 0},
             58} =
-             Parser.file_descriptor(@gd_scene)
+             Parser.file_descriptor_parser(@gd_scene)
   end
 
   test "parses node descriptor correctly" do
     assert {:ok, ["[node name=\"", "NodeName\" type=\"NodeType\"]"], "", %{}, {1, 0}, 38} =
              Parser.nodes_parser(@node_descriptor)
+  end
+
+  test "parses sub_resource descriptor correctly" do
+    assert {:ok, ["[sub_resource type=\"", "YourType\" id=\"YourID\"]"], "", %{}, {1, 0}, 42} =
+             Parser.sub_resource_parser(@sub_resource)
+  end
+
+  test "parses color correctly" do
+    assert {:ok, ["[color r=", "255 g=255 b=255 a=255]"], "", %{}, {1, 0}, 31} =
+             Parser.color_parser(@color)
+  end
+
+  test "parses transform correctly" do
+    assert {:ok, ["[transform cell0=", "1 cell1=0 cell2=0]"], "", %{}, {1, 0}, 35} =
+             Parser.transform_parser(@transform)
   end
 end
