238a023ef1b331ee09a24ae3bf22a341388d8867
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-14 01:14:35 -0800

    Refactor node parsing tests

--- test/elixir_godot_parsers_test.exs
+++ test/elixir_godot_parsers_test.exs
@@ -36,4 +36,17 @@ defmodule ElixirGodotParsersTest do
     assert {:ok, ["[transform cell0=", "1 cell1=0 cell2=0]"], "", %{}, {1, 0}, 35} =
              Parser.transform_parser(@transform)
   end
+
+  @omni_light "[node name=\"OmniLight3D\" type=\"OmniLight3D\" parent=\".\"] light_color = Color(1, 0.698039, 0.321569, 1) omni_range = 10.0"
+  @camera "[node name=\"Camera3D\" type=\"Camera3D\" parent=\".\"] transform = Transform3D(1, 0, 0, 0, 0.939693, 0.34202, 0, -0.34202, 0.939693, 0, 1, 3)"
+
+  test "parses OmniLight3D node correctly" do
+    assert {:ok, ["[node name=\"", "OmniLight3D\" type=\"OmniLight3D\" parent=\".\"] light_color = Color(1, 0.698039, 0.321569, 1) omni_range = 10.0"], "", %{}, {1, 0}, 119} =
+             Parser.nodes_parser(@omni_light)
+  end
+
+  test "parses Camera3D node correctly" do
+    assert {:ok, ["[node name=\"", "Camera3D\" type=\"Camera3D\" parent=\".\"] transform = Transform3D(1, 0, 0, 0, 0.939693, 0.34202, 0, -0.34202, 0.939693, 0, 1, 3)"], "", %{}, {1, 0}, 136} =
+             Parser.nodes_parser(@camera)
+  end
 end
