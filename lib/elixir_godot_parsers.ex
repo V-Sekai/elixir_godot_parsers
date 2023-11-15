53d35481a3bb7d81bdd7f1fee3e9f6400c32329b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-15 14:27:04 -0800

    Restart from the simple.

--- lib/elixir_godot_parsers.ex
+++ lib/elixir_godot_parsers.ex
@@ -1,164 +1,13 @@
-defmodule ElixirGodotParsers.Bool do
-  @type t :: %__MODULE__{value: integer()}
-  defstruct value: 0
-end
-
-defmodule ElixirGodotParsers.Int do
-  @type t :: %__MODULE__{value: integer()}
-  defstruct value: 0
-end
-
-defmodule ElixirGodotParsers.Vector2 do
-  @type t :: %__MODULE__{x: float(), y: float()}
-  defstruct x: 0.0, y: 0.0
-end
-
-defmodule ElixirGodotParsers.Rect2 do
-  @type t :: %__MODULE__{x: float(), y: float(), width: float(), height: float()}
-  defstruct x: 0.0, y: 0.0, width: 0.0, height: 0.0
-end
-
-defmodule ElixirGodotParsers.Vector3 do
-  @type t :: %__MODULE__{x: float(), y: float(), z: float()}
-  defstruct x: 0.0, y: 0.0, z: 0.0
-end
-
-defmodule ElixirGodotParsers.Transform2D do
-  @type t :: %__MODULE__{x: Vector2.t(), y: Vector2.t(), origin: Vector2.t()}
-  defstruct x: %ElixirGodotParsers.Vector2{},
-            y: %ElixirGodotParsers.Vector2{},
-            origin: %ElixirGodotParsers.Vector2{}
-end
-
-defmodule ElixirGodotParsers.Plane do
-  @type t :: %__MODULE__{normal: Vector3.t(), d: float()}
-  defstruct normal: %ElixirGodotParsers.Vector3{}, d: 0.0
-end
-
-defmodule ElixirGodotParsers.Quaternion do
-  @type t :: %__MODULE__{x: float(), y: float(), z: float(), w: float()}
-  defstruct x: 0.0, y: 0.0, z: 0.0, w: 0.0
-end
-
-defmodule ElixirGodotParsers.AABB do
-  @type t :: %__MODULE__{position: Vector3.t(), size: Vector3.t()}
-  defstruct position: %ElixirGodotParsers.Vector3{}, size: %ElixirGodotParsers.Vector3{}
-end
-
-defmodule ElixirGodotParsers.Basis do
-  @type t :: %__MODULE__{
-          x: ElixirGodotParsers.Vector3.t(),
-          y: ElixirGodotParsers.Vector3.t(),
-          z: ElixirGodotParsers.Vector3.t()
-        }
-  defstruct x: %ElixirGodotParsers.Vector3{},
-            y: %ElixirGodotParsers.Vector3{},
-            z: %ElixirGodotParsers.Vector3{}
-end
-
-defmodule ElixirGodotParsers.Transform3D do
-  @type t :: %__MODULE__{
-          basis: ElixirGodotParsers.Basis.t(),
-          origin: ElixirGodotParsers.Vector3.t()
-        }
-  defstruct basis: %ElixirGodotParsers.Basis{}, origin: %ElixirGodotParsers.Vector3{}
-end
-
-defmodule ElixirGodotParsers.Color do
-  @type t :: %__MODULE__{r: float(), g: float(), b: float(), a: float()}
-  defstruct r: 0.0, g: 0.0, b: 0.0, a: 0.0
-end
-
-defmodule ElixirGodotParsers.NodePath do
-  @type t :: %__MODULE__{path: binary()}
-  defstruct path: ""
-end
-
-defmodule RID do
-  @type t :: %__MODULE__{id: integer()}
-  defstruct id: 0
-end
-
-defmodule ElixirGodotParsers.Object do
-  @type t :: %__MODULE__{class_name: binary(), properties: list()}
-  defstruct class_name: "", properties: []
-end
-
-defmodule ElixirGodotParsers.Dictionary do
-  @type t :: %__MODULE__{elements: list()}
-  defstruct elements: []
-end
-
-defmodule ElixirGodotParsers.Array do
-  @type t :: %__MODULE__{elements: list()}
-  defstruct elements: []
-end
-
-defmodule ElixirGodotParsers.RawArray do
-  @type t :: %__MODULE__{data: binary()}
-  defstruct data: <<>>
-end
-
-defmodule ElixirGodotParsers.Int32Array do
-  @type t :: %__MODULE__{data: list(integer())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.Int64Array do
-  @type t :: %__MODULE__{data: list(integer())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.Float32Array do
-  @type t :: %__MODULE__{data: list(float())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.Float64Array do
-  @type t :: %__MODULE__{data: list(float())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.StringArray do
-  @type t :: %__MODULE__{data: list(binary())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.Vector2Array do
-  @type t :: %__MODULE__{data: list(Vector2.t())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.Vector3Array do
-  @type t :: %__MODULE__{data: list(Vector3.t())}
-  defstruct data: []
-end
-
-defmodule ElixirGodotParsers.ColorArray do
-  @type t :: %__MODULE__{data: list(Color.t())}
-  defstruct data: []
-end
-
 defmodule ElixirGodotParsers do
   use AbnfParsec,
     abnf: """
-    ip = first dot second dot third dot fourth
-    dot = "."
-    dec-octet =
-      "25" %x30-35      /   ; 250-255
-      "2" %x30-34 DIGIT /   ; 200-249
-      "1" 2DIGIT        /   ; 100-199
-      %x31-39 DIGIT     /   ; 10-99
-      DIGIT                 ; 0-9
-    first = dec-octet
-    second = dec-octet
-    third = dec-octet
-    fourth = dec-octet
+    gd-scene = "[gd_scene load_steps=" load-steps " format=" format "]"
+    load-steps = 1*DIGIT ; one or more digits
+    format = 1*DIGIT ; one or more digits
     """,
-    unbox: ["dec-octet"],
-    ignore: ["dot"],
-    parse: :ip,
-    parse: :nodes
+    unbox: ["load-steps", "format"],
+    ignore: [" "],
+    parse: :gd_scene
 end
 
 defmodule ElixirGodot do
@@ -170,53 +19,8 @@ defmodule ElixirGodot do
 
   defp parse_line(line) do
     case line do
-      ~r/^\[node/ -> ElixirGodotParsers.parse(:nodes, line)
+      ~r/^\[gd_scene/ -> ElixirGodotParsers.parse(:gd_scene)
       _ -> {:ok, line}
     end
   end
 end
-
-    # """
-    #   file-descriptor = "[gd_scene load_steps=" load-steps " format=" format " uid=\"" uid "\"]"
-    #   load-steps = 1*DIGIT ; one or more digits
-    #   format = 1*DIGIT ; one or more digits
-    #   uid = 1*HEXDIG ; one or more hexadecimal digits
-
-    #   external-resource = "[ext_resource path=\"" path "\" type=\"" resource-type "\" id=" id "]"
-    #   path = *ALPHA "/" *ALPHA ; a simple path rule, you might need to adjust this
-    #   resource-type = 1*ALPHA ; one or more alphabetic characters
-    #   id = 1*DIGIT ; one or more digits
-
-    #   external-resource = "[ext_resource path=\"" path "\" type=\"" resource-type "\" id=" id "]"
-    #   path = *ALPHA "/" *ALPHA ; a simple path rule, you might need to adjust this
-    #   resource-type = 1*ALPHA ; one or more alphabetic characters
-    #   id = 1*DIGIT ; one or more digits
-
-    #   internal-resource = "[sub_resource type=\"" resource-type "\" id=" id "]"
-
-    #   connection = "[connection signal=\"" signal "\" from=\"" from "\" to=\"" to "\" method=\"" method "\"]"
-    #   signal = *CHAR
-    #   from = *CHAR
-    #   to = *CHAR
-    #   method = *CHAR
-
-    #   color = "Color(" r ", " g ", " b ", " a ")"
-    #   r = *DIGIT "." *DIGIT
-    #   g = *DIGIT "." *DIGIT
-    #   b = *DIGIT "." *DIGIT
-    #   a = *DIGIT "." *DIGIT
-
-    #   transform = "Transform3D(" cell-0 ", " cell-1 ", " cell-2 ", " cell-3 ", " cell-4 ", " cell-5 ", " cell-6 ", " cell-7 ", " cell-8 ", " cell-9 ", " cell-10 ", " cell-11 ")"
-    #   cell-0 = *DIGIT "." *DIGIT
-    #   cell-1 = *DIGIT "." *DIGIT
-    #   cell-2 = *DIGIT "." *DIGIT
-    #   cell-3 = *DIGIT "." *DIGIT
-    #   cell-4 = *DIGIT "." *DIGIT
-    #   cell-5 = *DIGIT "." *DIGIT
-    #   cell-6 = *DIGIT "." *DIGIT
-    #   cell-7 = *DIGIT "." *DIGIT
-    #   cell-8 = *DIGIT "." *DIGIT
-    #   cell-9 = *DIGIT "." *DIGIT
-    #   cell-10 = *DIGIT "." *DIGIT
-    #   cell-11 = *DIGIT "." *DIGIT
-    # """,
--- test/elixir_godot_parsers_test.exs
+++ test/elixir_godot_parsers_test.exs
@@ -3,17 +3,67 @@ defmodule ElixirGodotParsersTest do
 
   alias ElixirGodot, as: Parser
 
-  @gd_scene "[gd_scene load_steps=4 format=3 uid=\"uid://cecaux1sm7mo0\"]"
-  @node_descriptor "[node name=\"NodeName\" type=\"NodeType\"]"
-  @sub_resource "[sub_resource type=\"YourType\" id=\"YourID\"]"
-  @color "Color(255, 255, 255, 255)"
-  @transform "Transform3D(1, 0, 0, 0, 0.939693, 0.34202, 0, -0.34202, 0.939693, 0, 1, 3)"
+  defmodule ElixirGodotParsersTest do
+    use ExUnit.Case
 
-  test "tscn_parser/1" do
-    assert [ok: "[gd_scene load_steps=4 format=3 uid=\"uid://cecaux1sm7mo0\"]"] = Parser.tscn_parser(@gd_scene)
-    assert [ok: "[node name=\"NodeName\" type=\"NodeType\"]"] = Parser.tscn_parser(@node_descriptor)
-    assert [ok: "[sub_resource type=\"YourType\" id=\"YourID\"]"] = Parser.tscn_parser(@sub_resource)
-    assert [ok: "Color(255, 255, 255, 255)"] = Parser.tscn_parser(@color)
-    assert [ok: "Transform3D(1, 0, 0, 0, 0.939693, 0.34202, 0, -0.34202, 0.939693, 0, 1, 3)"] = Parser.tscn_parser(@transform)
+    alias ElixirGodot, as: Parser
+
+    @gd_resource """
+    [gd_scene load_steps=4 format=3 uid="uid://cecaux1sm7mo0"]
+
+    [sub_resource type="SphereShape3D" id="SphereShape3D_tj6p1"]
+
+    [sub_resource type="SphereMesh" id="SphereMesh_4w3ye"]
+
+    [sub_resource type="StandardMaterial3D" id="StandardMaterial3D_k54se"]
+    albedo_color = Color(1, 0.639216, 0.309804, 1)
+
+    [node name="Ball" type="RigidBody3D"]
+
+    [node name="CollisionShape3D" type="CollisionShape3D" parent="."]
+    shape = SubResource("SphereShape3D_tj6p1")
+
+    [node name="MeshInstance3D" type="MeshInstance3D" parent="."]
+    mesh = SubResource("SphereMesh_4w3ye")
+    surface_material_override/0 = SubResource("StandardMaterial3D_k54se")
+
+    [node name="OmniLight3D" type="OmniLight3D" parent="."]
+    light_color = Color(1, 0.698039, 0.321569, 1)
+    omni_range = 10.0
+
+    [node name="Camera3D" type="Camera3D" parent="."]
+    transform = Transform3D(1, 0, 0, 0, 0.939693, 0.34202, 0, -0.34202, 0.939693, 0, 1, 3)
+    """
+
+    test "tscn_parser/1" do
+      assert [
+               ok: "[gd_scene load_steps=4 format=3 uid=\"uid://cecaux1sm7mo0\"]",
+               ok: "",
+               ok: "[sub_resource type=\"SphereShape3D\" id=\"SphereShape3D_tj6p1\"]",
+               ok: "",
+               ok: "[sub_resource type=\"SphereMesh\" id=\"SphereMesh_4w3ye\"]",
+               ok: "",
+               ok: "[sub_resource type=\"StandardMaterial3D\" id=\"StandardMaterial3D_k54se\"]",
+               ok: "albedo_color = Color(1, 0.639216, 0.309804, 1)",
+               ok: "",
+               ok: "[node name=\"Ball\" type=\"RigidBody3D\"]",
+               ok: "",
+               ok: "[node name=\"CollisionShape3D\" type=\"CollisionShape3D\" parent=\".\"]",
+               ok: "shape = SubResource(\"SphereShape3D_tj6p1\")",
+               ok: "",
+               ok: "[node name=\"MeshInstance3D\" type=\"MeshInstance3D\" parent=\".\"]",
+               ok: "mesh = SubResource(\"SphereMesh_4w3ye\")",
+               ok: "surface_material_override/0 = SubResource(\"StandardMaterial3D_k54se\")",
+               ok: "",
+               ok: "[node name=\"OmniLight3D\" type=\"OmniLight3D\" parent=\".\"]",
+               ok: "light_color = Color(1, 0.698039, 0.321569, 1)",
+               ok: "omni_range = 10.0",
+               ok: "",
+               ok: "[node name=\"Camera3D\" type=\"Camera3D\" parent=\".\"]",
+               ok:
+                 "transform = Transform3D(1, 0, 0, 0, 0.939693, 0.34202, 0, -0.34202, 0.939693, 0, 1, 3)",
+               ok: ""
+             ] = Parser.tscn_parser(@gd_resource)
+    end
   end
 end
