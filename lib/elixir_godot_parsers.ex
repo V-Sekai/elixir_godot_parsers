34a925c35ec73bc0e4a6cda68d480f667502532d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-15 14:44:32 -0800

    Parse the uid too.

--- lib/elixir_godot_parsers.ex
+++ lib/elixir_godot_parsers.ex
@@ -1,11 +1,12 @@
 defmodule ElixirGodotParsers do
   use AbnfParsec,
     abnf: """
-    gd-scene = "[gd_scene load_steps=" load-steps " format=" format "]"
+    gd-scene = "[gd_scene load_steps=" load-steps " format=" format " uid=" uid "]"
     load-steps = 1*DIGIT ; one or more digits
     format = 1*DIGIT ; one or more digits
+    uid = *CHAR ; zero or more characters
     """,
-    unbox: ["load-steps", "format"],
+    unbox: ["load-steps", "format", "uid"],
     ignore: [" "],
     parse: :gd_scene
 end
@@ -19,7 +20,7 @@ defmodule ElixirGodot do
 
   defp parse_line(line) do
     case line do
-      ~r/^\[gd_scene/ -> ElixirGodotParsers.parse(:gd_scene)
+      ~r/^\[gd_scene/ -> ElixirGodotParsers.parse(line)
       _ -> {:ok, line}
     end
   end
