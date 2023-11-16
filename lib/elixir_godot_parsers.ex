4edbfd7cb3e19dfb18bdc0ff281090320c764e49
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-15 20:13:30 -0800

    Support parsing the descriptor.

--- lib/elixir_godot_parsers.ex
+++ lib/elixir_godot_parsers.ex
@@ -3,37 +3,51 @@ defmodule ElixirGodotParsers do
 
   defstruct [:load_steps, :format, :uid]
 
-  load_descriptor =
-    ignore(string("[gd_scene"))
+  defp not_space(<<char::utf8, _rest::binary>>, context, _, _) when char != ?\s, do: {:cont, context}
+  defp not_space(_, context, _, _), do: {:halt, context}
+
+  defp not_quote(<<?", _::binary>>, context, _, _), do: {:halt, context}
+  defp not_quote(_, context, _, _), do: {:cont, context}
+
+  gd_scene =
+    ignore(string("["))
+    |> repeat_while(utf8_char([]), {:not_space, []})
     |> ignore(string(" "))
+    |> wrap()
+    |> tag(:gd_scene)
 
   load_steps =
     ignore(string("load_steps="))
-    |> utf8_string([], min: 1, max: 10)
+    |> integer(min: 1)
     |> ignore(string(" "))
+    |> tag(:load_steps)
 
   format =
     ignore(string("format="))
-    |> utf8_string([], min: 1, max: 10)
+    |> integer(min: 1)
     |> ignore(string(" "))
+    |> tag(:format)
 
   uid =
     ignore(string("uid=\""))
-    |> utf8_string([], min: 1)
+    |> repeat_while(utf8_char([]), {:not_quote, []})
     |> ignore(string("\""))
+    |> tag(:uid)
 
-  file_descriptor = load_descriptor
-    |> concat(load_steps)
-    |> concat(format)
-    |> concat(uid)
+
+  file_descriptor =
+    gd_scene
+    |> optional(load_steps)
+    |> optional(format)
+    |> optional(uid)
+    |> tag(:file_descriptor)
 
   external_resources = utf8_string(empty(), min: 1)
   internal_resources = utf8_string(empty(), min: 1)
   nodes = utf8_string(empty(), min: 1)
   connections = utf8_string(empty(), min: 1)
 
-  document =
-    optional(file_descriptor)
+  document = file_descriptor
     |> optional(external_resources)
     |> optional(internal_resources)
     |> optional(nodes)
--- test/elixir_godot_parsers_test.exs
+++ test/elixir_godot_parsers_test.exs
@@ -1,19 +1,13 @@
 defmodule ElixirGodotParsersTest do
   use ExUnit.Case
 
-  describe "echo_string/1" do
-    test "returns the original string when parsing is successful" do
-      assert {:ok, ["[gd_scene load_steps=5 format=3 uid=\"uid://b2agmmarkv5l3\"]"], "", %{},
-              {1, 0},
-              58} ==
-               ElixirGodotParsers.tscn(
-                 "[gd_scene load_steps=5 format=3 uid=\"uid://b2agmmarkv5l3\"]"
-               )
+  describe "tscn/1" do
+    test "returns an error when parsing fails" do
+      assert {:error, _, _, _, _, _} = ElixirGodotParsers.tscn("")
     end
 
-    test "returns an error when parsing fails" do
-      # Assuming that echo_parser cannot parse an empty string
-      assert {:ok, [], "", %{}, {1, 0}, 0} = ElixirGodotParsers.tscn("")
+    test "parsing is successful" do
+      assert {:ok, [{:file_descriptor, [{:gd_scene, [~c"gd_scene"]}, {:load_steps, [5]}, {:format, [3]}, {:uid, ~c"uid://b2agmmarkv5l3"}]}, "]"], "", %{}, {1, 0}, 58} = ElixirGodotParsers.tscn("[gd_scene load_steps=5 format=3 uid=\"uid://b2agmmarkv5l3\"]")
     end
   end
 end
