ee427d9bea20d981ad79fc77a7706e7461798e01
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-15 18:42:34 -0800

    Restructure ElixirGodotParsers module and update tests

--- lib/elixir_godot_parsers.ex
+++ lib/elixir_godot_parsers.ex
@@ -1,7 +1,19 @@
 defmodule ElixirGodotParsers do
   import NimbleParsec
 
-  document = utf8_string(empty(), min: 1)
+  defstruct [:file_descriptor, :external_resources, :internal_resources, :nodes, :connections]
 
-  defparsec :tscn, document, debug: true
+  file_descriptor = utf8_string(empty(), min: 1)
+  external_resources = utf8_string(empty(), min: 1)
+  internal_resources = utf8_string(empty(), min: 1)
+  nodes = utf8_string(empty(), min: 1)
+  connections = utf8_string(empty(), min: 1)
+
+  document = optional(file_descriptor)
+    |> optional(external_resources)
+    |> optional(internal_resources)
+    |> optional(nodes)
+    |> optional(connections)
+
+  defparsec :tscn, document, debug: false
 end
--- test/elixir_godot_parsers_test.exs
+++ test/elixir_godot_parsers_test.exs
@@ -1,8 +1,6 @@
 defmodule ElixirGodotParsersTest do
   use ExUnit.Case
 
-  alias ElixirGodotParsers, as: Parser
-
   describe "echo_string/1" do
     test "returns the original string when parsing is successful" do
       assert {:ok, ["hello world"], "", %{}, {1, 0}, 11} == ElixirGodotParsers.tscn("hello world")
@@ -10,7 +8,7 @@ defmodule ElixirGodotParsersTest do
 
     test "returns an error when parsing fails" do
       # Assuming that echo_parser cannot parse an empty string
-      assert {:error, "expected utf8 codepoint", "", %{}, {1, 0}, 0} = ElixirGodotParsers.tscn("")
+      assert {:ok, [], "", %{}, {1, 0}, 0} = ElixirGodotParsers.tscn("")
     end
   end
 end
